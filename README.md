25f8d257ef884b31d4a9b70a91944228819def92
Michela Qyteza <michelaq1997@gmail.com>
2021-05-20 19:03:03 -0400

    First Upload

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# ReadMe-Generator
+
+  # **My Read-Me**
+  [![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)
+
+  ## Description
+  blAH blah 
+
+  ## Table of Contents  
+  -[Description](#Description)  
+  -[Installation](#Installation)  
+  -[Usage](#Usage)  
+  -[License](#License)  
+  -[Contributors](#Contributors)  
+  -[Test](#Test)  
+  -[GitHub](#GitHub)  
+  -[Repository](#Repository)  
+
+  ## Installation
+  blah
+
+  ## Usage
+   bdjkdv
+
+  ## Contributors
+  sdv
+
+  ## Test
+  sdv
+
+  ## GitHub
+  sb
+
+  ## Repository
+  - [Project Repo](sb)
+  
---
+++ index.js
@@ -0,0 +1,77 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown =  require("./utils/generateMarkdown.js");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type:"input",
+        message:"Enter your projects title",
+        name:"Title"
+    },
+    {
+        type:"input",
+        message:"Projects description",
+        name:"Description"
+    },
+    {
+        type:"input",
+        message:"Provide installation instructions",
+        name:"Installation"
+    },
+    {
+        type:"input",
+        message:"Provide project usage",
+        name:"Usage"
+    },
+    {
+        type:"list",
+        message:"Provide license or badge link",
+        name:"License",
+        choices: ["Apache 2.0 License", "BSD 3-Clause License", "BSD 2-Clause License", "Attribution 4.0 International", "Attribution-ShareAlike 4.0 International", "Attribution-NonCommercial 4.0 International", "Attribution-NoDerivates 4.0 International", "Attribution-NonCommmercial-ShareAlike 4.0 International", "Attribution-NonCommercial-NoDerivatives 4.0 International", "Eclipse Public License 1.0", "GNU GPL v3","GNU GPL v2", "GNU AGPL v3", "GNU LGPL v3", "GNU FDL v1.3", "IBM Public License Version 1.0", "The MIT License", "Mozilla Public License 2.0", "The zlib/libpng License"]
+    },
+    {
+        type:"input",
+        message:"Provide contributing parties",
+        name:"Contributors"
+    },
+    {
+        type:"input",
+        message:"Provide project tests",
+        name:"Test"
+    },
+    {
+        type:"input",
+        message:"What is your GitHub username?",
+        name:"GitHub"
+    },
+    {
+        type:"input",
+        message:"What is your repository link?",
+        name:"Repository"
+    }
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data ,(err) =>
+        err ? console.error(err) : console.log('Success!')
+ );
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+   inquirer.prompt(questions)
+        .then(function(answers){
+           const text =  generateMarkdown.generateMarkdown(answers)
+           writeToFile("README.md",text)
+        })
+}
+
+// Function call to initialize app
+init();
+
+
